d4aba440268336a352e52e483f80663d2f7eb167
Kacper Krawczyk <kacper.krawczyk@beclose.eu>
2020-05-18 13:46:27 +0200

    Updates

---
+++ precache-manifest.d7d3165c0ef3114be87418a3583b5dc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2852a11de640534d77ac9ffcc0d1a0e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbb74ae395d4791c4dfb",
+    "url": "/static/css/main.fc08a226.chunk.css"
+  },
+  {
+    "revision": "694646dc52519fd780e8",
+    "url": "/static/js/2.dbee4b71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbee4b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb74ae395d4791c4dfb",
+    "url": "/static/js/main.b56ee242.chunk.js"
+  },
+  {
+    "revision": "f315b4962ad976115dce",
+    "url": "/static/js/runtime-main.d9501914.js"
+  },
+  {
+    "revision": "106156cdaef9ab274d25190b7b439870",
+    "url": "/static/media/arrowLeft.106156cd.svg"
+  },
+  {
+    "revision": "dd29e0d4aba51c31d8801718238c43f6",
+    "url": "/static/media/arrowRight.dd29e0d4.svg"
+  },
+  {
+    "revision": "afe3c55974177494f1a853a131416c2f",
+    "url": "/static/media/directionArrow.afe3c559.svg"
+  }
+]);
